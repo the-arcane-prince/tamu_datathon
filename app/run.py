e8d9fa7d6645f179f43a6c6e0f3ea90222b7b54d
neilshirsat <neilshirsat2015@gmail.com>
2024-11-09 14:13:46 -0600

    init

---
+++ app/run.py
@@ -0,0 +1,17 @@
+
+from flask import Flask, request
+from game import game
+
+app = Flask(__name__)
+
+@app.post("/")
+def challengeSetup():
+    req = request.get_json()
+    words = req['words']
+    strikes = req['strikes']
+    is_one_away = req['isOneAway']
+    correct_groups = req['correctGroups']
+    previous_guesses = req['previousGuesses']
+    error = req['error']
+
+    guess, endTurn = game.model(words, strikes, is_one_away, correct_groups, previous_guesses, error)
---
+++ app/word_association.py
@@ -0,0 +1,6 @@
+class WordAssociations:
+    def compute_word_associations(words):
+        pass
+    
+    
+    
