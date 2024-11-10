a98f94279537bdeedc925204d6baa6f10589df16
neilshirsat <neilshirsat2015@gmail.com>
2024-11-10 01:43:48 -0600

    Added the configuration to load data

--- app/lib/test_word_association.py
+++ app/lib/test_word_association.py
@@ -1,10 +1,10 @@
 import pytest
 from . import word_association as wa
+from .training_data.load_data import load_all_data
 
 def test_sample():
     print("Hello World")
 
 def test_word_association():
     x = wa.WordAssociations()
-    e = x.compute_embeddings("Hello")
     print(e)
