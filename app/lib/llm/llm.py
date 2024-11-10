215b79751d95e3fd4fcf7d72fc06d07279a24abc
neilshirsat <neilshirsat2015@gmail.com>
2024-11-10 02:02:57 -0600

    Configured the llm to run locally on the same process. In order to speed things up later we need to move the model inferencing into its own seperate process

--- app/lib/llm/llm.py
+++ app/lib/llm/llm.py
@@ -6,6 +6,12 @@ A basic large language model that can be used for the purpose
 of querying relational data from natural language
 
 BASE MODEL: LLAMA 3.1 70B; 128K Context Window
+
+TODO: Move the inference to a separate process so the inference
+does not block the main process
+    - Make the external process contain the chat history for the
+      conversation and include a clear history switch in order to
+      reset the conversation buffer
 """
 class LLM:
     
@@ -13,16 +19,13 @@ class LLM:
     Create, intialize, and load the model for inferencing
     """
     def __init__(self):
-        # self.model = llama.Llama(
-        #     model_path=os.getenv('LLAMA_MODEL_PATH')
-        # )
         self.model = llama.Llama.from_pretrained(
 	        repo_id="neil-shirsat/Llama-3.1-8B-Q8_0-GGUF",
 	        filename="llama-3.1-8b-q8_0.gguf",
         )
         
     """
-    Create a request to generate text from the remote source
+    Create a llm request to the llm
     """
     def generate_text(self, prompt):
         return self.model(
@@ -36,4 +39,9 @@ class LLM:
     Create a chat completion to the external llm
     """
     def generate_chat_completion(self, prompt):
-        pass
+        return self.model.generate(
+            prompt=prompt,
+            max_tokens=128000,
+            stop=["Q:"],
+            echo=False
+        )
